e156676cbcab037671bd395ba5a8326f481d54e8
Henry Muru Paenga <meringu@gmail.com>
2016-06-27 10:40:34 +1200

    0.2.0

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ source_url       'https://github.com/meringu/plex'
 issues_url       'https://github.com/meringu/plex/issues'
 
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
 supports         'ubuntu', '>= 10.04'
 supports         'fedora', '>= 14'
 supports         'centos', '>= 6'
